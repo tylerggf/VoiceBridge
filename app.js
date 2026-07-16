7a9779922ed07526f3326bde75d3715169021e7a
tylerggf <162388664+tylerggf@users.noreply.github.com>
2026-07-16 08:19:26 -0700

    Update route handling for static files and catch-all

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve your static files
 app.use(express.static(__dirname));
 
-app.get("*", (req, res) => {
+// Catch all routes (Express 5 compatible)
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
